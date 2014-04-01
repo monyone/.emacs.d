0a6501a6e72341d5a2369a4346de079b95c64214
monyone <monyone.teihen@gmail.com>
2014-04-01 12:24:27 +0900

    reduce を使う為に require を cl から cl-lib へ変更

--- init.el
+++ init.el
@@ -102,7 +102,7 @@
   "A list of packages to install from package at launch.")
 ;; 無いものに関してはインストールするように.
 ; 未インストールなものが無いかチェック
-(require 'cl)
+(require 'cl-lib)
 (setq already-installed-packages-p
   (reduce (lambda (ret package) (and ret (if (eq 't package) 't (package-installed-p package)))) (append '(t t) installed_packages)))
 
