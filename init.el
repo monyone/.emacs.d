2949d007130e180f2220b1ee8a869f7820a4fd5f
monyone <monyone.teihen@gmail.com>
2015-07-14 21:12:35 +0900

    rainbow-delimiters の色を強調して表示する設定に

--- init.el
+++ init.el
@@ -147,8 +147,16 @@
 
 ;; rainbow-delimiters
 (require 'rainbow-delimiters)
+(require 'color)
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-(custom-set-faces '(rainbow-delimiters-depth-1-face ((t (:foreground "#7f8c8d")))))
+(defun rainbow-delimiters-using-stronger-colors() 
+	(interactive)
+	(cl-loop
+	 for index from 1 to rainbow-delimiters-max-face-count
+	 do
+	 (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+		 (cl-callf color-saturate-name (face-foreground face) 30))))
+(add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors)
 
 ;; sudden-death
 (require 'sudden-death)
